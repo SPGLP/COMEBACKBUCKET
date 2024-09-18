beaf870b4574b50c44fcae8ae8cfd3ed0640e08f
17392 <1739255689@qq.com>
2024-09-18 14:37:42 +0800

    🧰 软件项目目录和构架方式优化

--- COMEBACKBUCKET/ViewModels/DataManagerViewModel.cs
+++ COMEBACKBUCKET/ViewModels/DataManagerViewModel.cs
@@ -1,10 +1,19 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Drawing.Imaging;
+using System.Drawing.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using COMEBACKBUCKET.Views.Components.DM;
 
 namespace COMEBACKBUCKET.ViewModels;
 
 public partial class DataManagerViewModel : ObservableRecipient
 {
+    private Type _workComponent;
+
+    public Type WorkComponent {get; set;}
+
     public DataManagerViewModel()
     {
+        WorkComponent = typeof(DM_Main);
     }
 }
